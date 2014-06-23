a37e604f1bf0cbaf5bccc43608a858ea01cbb54c
Heedong Lee <arkinylee@gmail.com>
2014-06-23 23:02:29 +0900

    Do Some hw

--- 003 변수 Variable/001 변수의 종류/main.cpp
+++ 003 변수 Variable/001 변수의 종류/main.cpp
@@ -2,12 +2,15 @@
 	변수 Variable
 	- 변할 수 있는 수
 	- 상수, 변수를 담을수 있는 공간 (방...?!)
-	- 변수마다 다른 크기를 같는다. 
+	- 변수마다 다른 크기를 갖는다. 
 
-	int			데이타의 크기(byte), 범위?
-	char		
-	float
-	double
+					C++ Data Types Ranges
+
+				Typical Bit width		Typical Range
+	int			4 bytes					-2147483648 to 2147483647
+	char		1 byte					-127 to 127 or 0 to 255(whine compiled by using /j)
+	float		4 bytes					+/- 3.4e +/- 38 (~7 digits)
+	double		8 bytes					+/- 1.7e +/- 308 (~15 digits)
 
 	ppt 혹은 이미지 스크린 샷 해서 과제 폴더
 */
