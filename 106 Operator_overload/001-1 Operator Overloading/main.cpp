3c95047bd97322c4e2d8a6bbf427cca30a1c8b4d
Heedong Lee <arkinylee@gmail.com>
2014-08-08 05:33:48 +0900

    08082014 Homework

--- 106 Operator_overload/001-1 Operator Overloading/main.cpp
+++ 106 Operator_overload/001-1 Operator Overloading/main.cpp
@@ -1,4 +1,8 @@
 /*
+	@author		Heedong Arkiny Lee
+	@date		08082014
+	@git		https://github.com/arkiny/SGA-Learning-Heedong
+
 	Operator 연산자
 	= + -
 	>> <<
@@ -37,7 +41,7 @@ typedef class point{
 public:
 	// 생성자 호출과 동시에 초기화
 	point(float x, float y){
-		cout << "--- constructor 3 ---" << endl;
+		//cout << "--- constructor 3 ---" << endl;
 		fX = x;
 		fY = y;
 	}
@@ -49,6 +53,10 @@ public:
 	point& operator-(const point &rhs);
 	point& operator*(const point &rhs);
 	point& operator/(const point &rhs);
+
+	bool operator==(const point &rhs);
+	float operator[](const int &in);
+	point& operator!();
 
 	void operator()();
 
@@ -96,6 +104,30 @@ void point::operator()(){
 	this->show();
 }
 
+bool point::operator==(const point &rhs){
+	return fX == rhs.fX && fY == rhs.fY;
+}
+float point::operator[](const int &in){
+	switch (in)
+	{
+	case 1:
+		return fX;
+		break;
+	case 2:
+		return fY;
+		break;
+	default:
+		return NULL;
+		break;
+	}
+}
+
+point& point::operator!(){
+	fX = -fX;
+	fY = -fY;
+	return *this;
+}
+
 void point::show(){
 	cout << "fX = " << fX << ", fY = " << fY << endl;
 }
@@ -106,10 +138,26 @@ main
 void main(){
 	POINT pt(1.2f, 7.8f);
 	POINT pt2(5.5f, 6.6f);
+	POINT pt3(1.2f, 7.8f);
 
-	pt = pt + pt2;
+	//pt = pt + pt2;
+	//pt.show();
+	//pt();
+	//bool bflag = (pt == pt2);
+	//float fx = pt[0]; // 배열처럼 쓰는데, 내부의 fX를 리턴
+	//float fy = pt[1]; // 내부의 fY를 리턴
+	//pt[2]->error 처리
+	//	!pt // 부호바꾸기
+	bool bflag = (pt == pt2);
+	bool bflag2 = (pt == pt3);
+	float fx = pt[0];
+	float fy = pt[1];
+	pt = !pt;
 
-	pt.show();
-	pt();
+	cout << "(pt == pt2) = " << bflag << endl; // 0
+	cout << "(pt == pt3) = " << bflag2 << endl; // 1
+	cout << "pt[0] = " << fx << endl; // 1.2
+	cout << "pt[1] = " << fy << endl; // 7.8
+	cout << "!pt = "; pt.show(); cout << endl; // -1.2, -7.8
 
 }
